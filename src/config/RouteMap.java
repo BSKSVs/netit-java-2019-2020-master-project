60829cd02717f06479148903bd361d060eeefc1b
Mihail Petrov <mail@mihail-petrov.me>
2020-07-13 21:26:36 +0300

    Add product list route

--- src/config/RouteMap.java
+++ src/config/RouteMap.java
@@ -2,8 +2,9 @@ package config;
 
 public interface RouteMap {
 
-    String HOME = "/base/home/index";
-    String PRODUCT_NOT_FOUND = "/base/product/notfound";
+    String HOME                 = "/base/home/index";
+    String PRODUCT_LIST         = "/base/product/list";
+    String PRODUCT_NOT_FOUND    = "/base/product/notfound";
 }
 
 
