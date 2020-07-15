553af47c3032e3af7d95b652d93d6ab1d0cc0f9e
Mihail Petrov <mail@mihail-petrov.me>
2020-07-15 21:14:41 +0300

    Add new entries to map files

--- src/config/RouteMap.java
+++ src/config/RouteMap.java
@@ -4,6 +4,7 @@ public interface RouteMap {
 
     String HOME                 = "/base/home/index";
     String PRODUCT_LIST         = "/base/product/list";
+    String PRODUCT_SEARCH       = "/base/product/search";
     String PRODUCT_NOT_FOUND    = "/base/product/notfound";
 }
 
